3ae7fecd6ffd5e0195ba3d0f88d48fd73070059e
gautamguptagit <gautamguptamit@gmail.com>
2022-07-14 14:16:35 +0000

    gg

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=dockdockgg/ggdock
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username dockdockgg
+docker tag ml-kubermnetes $dockerpath
 
 # Step 3:
-# Push image to a docker repository
+# Push image to a docker repository.
+docker push $dockerpath
